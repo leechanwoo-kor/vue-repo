2a635cd41e1ff9e9a0e01b68eb3ef22e80c29984
leechanwoo-kor <cwlee1124@gmail.com>
2021-01-04 15:14:20 +0900

    test

---
+++ my-project/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import HelloWorld0 from '@/components/HelloWorld0'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'HelloWorld0',
+      component: HelloWorld0
+    }
+  ]
+})
