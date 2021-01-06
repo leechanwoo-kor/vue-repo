5b9b5052e649217ba4c9e73d0d6a0a399cfd4a56
leechanwoo-kor <cwlee1124@gmail.com>
2021-01-06 11:30:23 +0900

    board bcrypt commit

--- board/models/User.js
+++ board/models/User.js
@@ -1,8 +1,10 @@
 // models/User.js
 
 var mongoose = require('mongoose');
+var bcrypt = require('bcryptjs'); // 1
 
-// schema // 1
+
+// schema
 var userSchema = mongoose.Schema({
   username:{type:String, required:[true,'Username is required!'], unique:true},
   password:{type:String, required:[true,'Password is required!'], select:false},
@@ -12,7 +14,7 @@ var userSchema = mongoose.Schema({
   toObject:{virtuals:true}
 });
 
-// virtuals // 2
+// virtuals
 userSchema.virtual('passwordConfirmation')
   .get(function(){ return this._passwordConfirmation; })
   .set(function(value){ this._passwordConfirmation=value; });
@@ -29,12 +31,12 @@ userSchema.virtual('newPassword')
   .get(function(){ return this._newPassword; })
   .set(function(value){ this._newPassword=value; });
 
-// password validation // 3
+// password validation
 userSchema.path('password').validate(function(v) {
-  var user = this; // 3-1
+  var user = this;
 
-  // create user // 3-3
-  if(user.isNew){ // 3-2
+  // create user
+  if(user.isNew){
     if(!user.passwordConfirmation){
       user.invalidate('passwordConfirmation', 'Password Confirmation is required.');
     }
@@ -44,12 +46,12 @@ userSchema.path('password').validate(function(v) {
     }
   }
 
-  // update user // 3-4
+  // update user
   if(!user.isNew){
     if(!user.currentPassword){
       user.invalidate('currentPassword', 'Current Password is required!');
     }
-    else if(user.currentPassword != user.originalPassword){
+    else if(!bcrypt.compareSync(user.currentPassword, user.originalPassword)){ // 2
       user.invalidate('currentPassword', 'Current Password is invalid!');
     }
 
@@ -58,6 +60,24 @@ userSchema.path('password').validate(function(v) {
     }
   }
 });
+
+// hash password // 3
+userSchema.pre('save', function (next){
+  var user = this;
+  if(!user.isModified('password')){ // 3-1
+    return next();
+  }
+  else {
+    user.password = bcrypt.hashSync(user.password); //3-2
+    return next();
+  }
+});
+
+// model methods // 4
+userSchema.methods.authenticate = function (password) {
+  var user = this;
+  return bcrypt.compareSync(password,user.password);
+};
 
 // model & export
 var User = mongoose.model('user',userSchema);
