886f316b079484a0f4ad5a7a3331656015bf33c9
leechanwoo-kor <cwlee1124@gmail.com>
2021-01-06 11:22:56 +0900

    board added users

--- board/index.js
+++ board/index.js
@@ -29,6 +29,8 @@ app.use(methodOverride('_method'));
 
 // Routes
 app.use('/', require('./routes/home'));
+app.use('/posts', require('./routes/posts'));
+app.use('/users', require('./routes/users')); // 1
 
 // Port setting
 var port = 3000;
